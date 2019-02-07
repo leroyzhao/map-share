6212bc77b7119e5a3fe1241639629d5df3f341ca
leroyzhao <leroyzhao99@gmail.com>
2019-02-07 12:08:43 -0500

    initilize basic routes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
+const HTTP_PORT = process.env.PORT || 3000;
 const express = require("express");
-const HTTP_PORT = process.env.PORT || 4000;
 
 //set up express app
 const app = express();
+
+// initialize routes
+app.use('/api', require('./routes/api'));
 
 // listen for requests
 app.listen(HTTP_PORT, () => {
---
+++ backend/routes/api.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+// get list of restaurants from db
+router.get('/restaurants', (req,res) => {
+  res.send({type: 'GET'});
+});
+
+// add new restaurant to db
+router.post('/restaurants', (req,res) => {
+  res.send({type: 'POST'});
+});
+
+// get restaurant from db
+router.get('/restaurants/:id', (req,res) => {
+  res.send({type: 'GET'});
+});
+
+// update restaurant in db
+router.put('/restaurants/:id', (req,res) => {
+  res.send({type: 'PUT'});
+});
+
+// delete restaurant in db
+router.delete('/restaurants/:id', (req,res) => {
+  res.send({type: 'DELETE'});
+});
+
+module.exports = router;
