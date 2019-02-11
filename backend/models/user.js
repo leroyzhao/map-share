1f02dd20f28ea1af3c66e4a018d0af6dc5b9bda0
leroyzhao <leroyzhao99@gmail.com>
2019-02-11 13:53:45 -0500

    create user schema

---
+++ backend/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// crete ninja Schema & model
+const UserSchema = new Schema({
+  userId: {
+    type: String,
+    required: [true, 'unique userId is required to save user / OR USE _OID????']
+  },
+  userEmail: {
+    type: String,
+    required: [true, 'Name field is required']
+  },
+  userFirstName: {
+    type: String,
+    required: [true, 'user firstName is required']
+  },
+  userLastName: {
+    type: String,
+    required: [true, 'user lastName is required']
+  },
+  userGroupId: String
+})
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
