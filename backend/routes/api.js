9afc30eb2eaaa52c23408ba9bb04f77202d30d8b
leroyzhao <leroyzhao99@gmail.com>
2019-02-11 16:25:36 -0500

    add more routes

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -9,10 +9,13 @@ router.get('/restaurants', (req,res) => {
 
 // add new restaurant to db
 router.post('/restaurants', (req,res,next) => {
-  Restaurant.create(req.body).then((restaurant) => {
-    res.send(restaurant);
-  }).catch(next);
+  // Restaurant.create(req.body).then((restaurant) => {
+  //   res.send(restaurant);
+  // }).catch(next);
+  res.send({'body': req.body});
 });
+
+/////////////////////////RESTAURANTS///////////////////////////////
 
 // get restaurant from db
 router.get('/restaurants/:id', (req,res,next) => {
@@ -32,5 +35,69 @@ router.delete('/restaurants/:id', (req,res,next) => {
   });
   res.send({type: 'DELETE'});
 });
+
+/////////////////////////REVIEWS///////////////////////////////
+
+// user adds review
+router.post('/reviews/:id', (req,res,next) => {
+  res.send({type: 'POST REVIEW'});
+});
+
+// user edits review???
+router.put('/reviews/:id', (req,res,next) => {
+  res.send({type: 'PUT REVIEW'});
+});
+
+/////////////////////////MARKS///////////////////////////////
+
+// get all marks to populate map
+router.get('/marks', (req,res,next) => {
+  res.send({type: 'GET all marks'});
+});
+
+/////////////////////////USER///////////////////////////////
+
+// get all users???
+router.get('/users', (req,res,next) => {
+  res.send({type: 'GET all users'});
+});
+
+// add new user
+router.post('/users/:userId', (req,res,next) => {
+  res.send({type: 'POST user'});
+});
+
+/////////////////////////CUISINE///////////////////////////////
+
+// provide options for cuisines?
+router.get('/cuisines', (req,res,next) => {
+  res.send({type: 'GET all cuisines'});
+});
+
+// get restaurants of specific cuisine
+router.get('/cuisines/:cuisine', (req,res,next) => {
+  res.send({type: 'GET restaurants of specific cuisine'});
+});
+
+/////////////////////////PRICE RANGE///////////////////////////////
+
+// get restaurants of specific price range
+router.get('/priceRanges/:priceRange', (req,res,next) => {
+  res.send({type: 'GET restaurants of specific price range'});
+});
+
+/////////////////////////GROUPS////////////////////////////////////
+
+//list groups
+router.get('/groups', (req,res,next) => {
+  res.send({type: 'GET list of group names'});
+});
+
+//list members of group
+router.get('/groups/:groupId', (req,res,next) => {
+  res.send({type: 'GET members of specific group'});
+});
+
+///////////////////////////////////////////////////////////////////
 
 module.exports = router;
