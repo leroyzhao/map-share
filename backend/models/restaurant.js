0437124bfe0edfbbf0f6cbe145dd0d52a9b22283
leroyzhao <leroyzhao99@gmail.com>
2019-02-11 11:37:16 -0500

    updated restaurant schema

--- backend/models/restaurant.js
+++ backend/models/restaurant.js
@@ -3,17 +3,32 @@ const Schema = mongoose.Schema;
 
 // crete ninja Schema & model
 const RestaurantSchema = new Schema({
+  mark_id: {
+    type: String,
+    required: [true, 'unique mark_id is required to save restaurant']
+  },
   name: {
     type: String,
     required: [true, 'Name field is required']
   },
   location: {
     type: String,
+    required: [true, 'Location (string) is required']
   },
-  available: {
-    type: Boolean,
-    default: false
+  cuisine: {
+    type: String,
+  },
+  price_range: {
+    type: Number,
   }
+  // total_reviews: {
+  //   type: Number,
+  //   default: 0,
+  // },
+  // average_rating: {
+  //   type: Number,
+  //   default: 0,
+  // }
 })
 
 const Restaurant = mongoose.model('restaurant', RestaurantSchema);
