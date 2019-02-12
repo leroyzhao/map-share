f72f1db37c14a894542bc2daaaba070fa7cce28e
leroyzhao <leroyzhao99@gmail.com>
2019-02-12 18:42:20 -0500

    add getRestaurants to dataService

--- backend/dataService.js
+++ backend/dataService.js
@@ -1,9 +1,42 @@
 const mongoose = require("mongoose");
+const ObjectId = mongoose.Types.ObjectId
+
+const Restaurant = require('./models/restaurant');
+const Cuisine = require('./models/cuisine');
+const Mark = require('./models/mark');
+const PriceRange = require('./models/priceRange');
+const Review = require('./models/review');
+const User = require('./models/user');
 
 module.exports = () => {
   return {
+
+    // ///// Function Outline! 
+    // functionName: () => {
+    //   return new Promise((resolve, reject) => {
+    //     SchemaName.what()
+    //     .then(data => {
+    //       resolve(data)
+    //     }).catch(err => {
+    //       reject(err)
+    //     });
+    //   });
+    // },
+
     getRestaurants: () => {
-      return []
-    }
+      return new Promise((resolve, reject) => {
+        Restaurant.find()
+        .then(data => {
+          console.log('data retrieved?', data)
+          resolve(data)
+        }).catch(err => {
+          console.log('ERROROROOR')
+          reject(err)
+        });
+      });
+    },
+
+
+
   }
 }
